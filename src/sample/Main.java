1e133fead8059d029b617aa1f3890f928d0233ee
ab3308BATH <75852160+ab3308BATH@users.noreply.github.com>
2023-02-14 18:55:04 +0000

    Select Image functionality

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,15 +4,37 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+
+import java.awt.*;
+import java.io.File;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Model Prediction Visualiser");
+        primaryStage.setScene(new Scene(root, 900, 500));
+
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select Image");
+
+        ImageView selectedImage = (ImageView) root.lookup("#imageView");
+
+        Button selectButton = (Button) root.lookup("#selectButton");
+        selectButton.setOnAction(event -> {
+            File imageFile = fileChooser.showOpenDialog(primaryStage);
+            if(imageFile != null && imageFile.exists()){
+                javafx.scene.image.Image image = new Image(imageFile.toURI().toString());
+                selectedImage.setImage(image);
+            }
+        });
+
         primaryStage.show();
     }
 
